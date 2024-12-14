e5b3bc15f0ef722d92422916573708db84d946ba
esse009 <esse.zeng@outlook.com>
2024-12-15 00:11:42 +0100

    update

--- display_emotions.py
+++ display_emotions.py
@@ -14,7 +14,7 @@ async def get_characteristic(client):
     interaction_characteristic = interaction_service.get_characteristic('FA02')
     await client.write_gatt_char(interaction_characteristic, bytes.fromhex("04000580"), True)
     await client.write_gatt_char(interaction_characteristic, bytes.fromhex("08000180173b1400"), True)
-    await client.write_gatt_char(interaction_characteristic, bytes.fromhex("0700088001000b", True))
+    await client.write_gatt_char(interaction_characteristic, bytes.fromhex("0700088001000b"), True)
     return interaction_characteristic
 
 async def display_neutral(client, characteristic):
