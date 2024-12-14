5a1938f2383c747320f9c18e541e470ac9fb19af
esse009 <esse.zeng@outlook.com>
2024-12-15 00:10:23 +0100

    update

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import time
 async def main():
     emotion_client = await display_emotions.get_client()
     emotion_char = await display_emotions.get_characteristic(emotion_client)
-    time.sleep(10)
     await display_emotions.display_angry(emotion_client, emotion_char)
     time.sleep(10)
     camera = emotion.get_camera()
