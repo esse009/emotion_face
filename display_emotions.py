b4a436e30629535e3a7ca5d8f0a6cfe465b63260
esse009 <esse.zeng@outlook.com>
2024-12-14 23:49:35 +0100

    chore: udpate

--- display_emotions.py
+++ display_emotions.py
@@ -12,7 +12,7 @@ async def get_client():
 async def get_characteristic(client):
     interaction_service = client.services.get_service('00FA')
     interaction_characteristic = interaction_service.get_characteristic('FA02')
-    await client.write_gatt_char(interaction_characteristic, bytes.fromhex("04000580"), True)
+    await client.write_gatt_char(interaction_characteristic, bytes.fromhex("0800018017301600"), True)
     return interaction_characteristic
 
 async def display_neutral(client, characteristic):
