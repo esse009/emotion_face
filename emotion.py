52acca9492d7c44063c8b0000824097d915566c8
esse009 <esse.zeng@outlook.com>
2024-12-14 23:29:37 +0100

    chore: setup emotions

--- emotion.py
+++ emotion.py
@@ -3,53 +3,35 @@ from deepface import DeepFace
 from picamera2 import Picamera2
 
 # Load face cascade classifier
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-# cv2.startWindowThread()
 
-# Start capturing video
-picam2 = Picamera2()
+def get_camera():
+    picam2 = Picamera2()
+    picam2.configure(picam2.create_video_configuration(main={"format": 'RGB888',
+                                                            "size": (640, 480)}))
+    picam2.start()
+    return picam2
 
-picam2.configure(picam2.create_video_configuration(main={"format": 'RGB888',
-                                                           "size": (640, 480)}))
+def cleanup_camera(camera):
+    camera.stop()
 
-picam2.start()
-while True:
-    # Capture frame-by-frame
-    frame = picam2.capture_array()
-    # Convert frame to grayscale
-    # gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def get_classifier():
+    return cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-    # # Convert grayscale frame to RGB format
-    # rgb_frame = cv2.cvtColor(gray_frame, cv2.COLOR_GRAY2RGB)
+def get_emotion(camera, classifier):
+    frame = camera.capture_array()
+    faces = classifier.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    if (len(faces) == 0):
+        return "neutral"
+    # We only analyse one face or we would get confused
+    (x, y, w, h) = faces[0]
+    # Extract the face ROI (Region of Interest)
+    face_roi = frame[y:y + h, x:x + w]
 
-    # # Detect faces in the frame
-    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    # Perform emotion analysis on the face ROI
+    result = DeepFace.analyze(face_roi, actions=['emotion'], enforce_detection=False)
 
-    for (x, y, w, h) in faces:
-        # Extract the face ROI (Region of Interest)
-        face_roi = frame[y:y + h, x:x + w]
+    # Determine the dominant emotion
+    emotion = result[0]['dominant_emotion']
+    return emotion
 
-        
-        # Perform emotion analysis on the face ROI
-        result = DeepFace.analyze(face_roi, actions=['emotion'], enforce_detection=False)
-
-        # Determine the dominant emotion
-        emotion = result[0]['dominant_emotion']
-        print(emotion)
-
-        # Draw rectangle around face and label with predicted emotion
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
-
-    # Display the resulting frame
-    # cv2.imshow('frame', frame)
-
-    # Press 'q' to exit
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break
-    # cv2.waitKey(1)
-
-# Release the capture and close all windows
-picam2.stop()
-cv2.destroyAllWindows()
 
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import asyncio
 import display_emotions
-
+import emotion
 
 
 async def main():
-    emotion_client = display_emotions.get_client()
-    emotion_char = display_emotions.get_characteristic(emotion_client)
+    emotion_client = await display_emotions.get_client()
+    emotion_char = await display_emotions.get_characteristic(emotion_client)
+    camera = emotion.get_camera()
+    classifier = emotion.get_classifier()
 
+    while True:
+        e = emotion.get_emotion(camera, classifier)
+        display_emotions.display_emotion(emotion_client, emotion_char, e)
     
+    emotion.cleanup_camera(camera)
 
 if __name__ == "__main__":
     asyncio.run(main)
