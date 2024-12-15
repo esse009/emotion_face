a830332765ba132718ccff9f35577e8b1038ce65
esse009 <esse.zeng@outlook.com>
2024-12-15 18:35:23 +0100

    g

--- servomotor.py
+++ servomotor.py
@@ -5,7 +5,7 @@ import asyncio
 
 kit = ServoKit(channels=16)
 
-def rotate_servo(channel, angle, speed):
+async def rotate_servo(channel, angle, speed):
 
     rotation_rate = 360  
     rotation_time = abs(angle) / (rotation_rate * abs(speed))  
@@ -13,7 +13,7 @@ def rotate_servo(channel, angle, speed):
 
     throttle = speed if angle > 0 else -speed
     kit.continuous_servo[channel].throttle = throttle
-    time.sleep(rotation_time)  
+    await asyncio.sleep(rotation_time)  
 
     
     kit.continuous_servo[channel].throttle = 0
@@ -25,24 +25,24 @@ async def move_by_emotion(emotion):
     for angle in config.movements[emotion]:
         await rotate_servo(15, angle, 1)
 
-try:
-    rotate_servo(15, 180, 1)  
-    rotate_servo(15, -180, 1)  
-    rotate_servo(15, 180, 1)  
-    rotate_servo(15, -180, 1)  
+# try:
+#     rotate_servo(15, 180, 1)  
+#     rotate_servo(15, -180, 1)  
+#     rotate_servo(15, 180, 1)  
+#     rotate_servo(15, -180, 1)  
 
 
 
 
-    rotate_servo(7, 53, 0.5)  
-    rotate_servo(7, -45, 0.5)
+#     rotate_servo(7, 53, 0.5)  
+#     rotate_servo(7, -45, 0.5)
 
-except Exception as e:
-    print(f"????: {e}")
+# except Exception as e:
+#     print(f"????: {e}")
 
-finally:
+# finally:
   
-    kit.continuous_servo[7].throttle = 0
+#     kit.continuous_servo[7].throttle = 0
   
   
 
