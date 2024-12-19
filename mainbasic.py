a420cb32b24b3086ae27f7fbea6571d3ca1f1c29
esse009 <esse.zeng@outlook.com>
2024-12-19 23:56:40 +0100

    s

--- mainbasic.py
+++ mainbasic.py
@@ -115,12 +115,12 @@ async def play_audio_after_delay_enter_highway(client, char):
     await servomotor.rotate_servo(config.HORIZONTAL, -60, 0.5)
 
     time = 0
-    while time < 41:
+    while time < 23:
         e = "neutral"
         # 显示 neutral 表情
         await display_emotions.display_emotion(client, char, e)
         # 等待 2 秒或直到 41 秒为止
-        await asyncio.sleep(min(41 - time, 2))
+        await asyncio.sleep(min(23 - time, 2))
         time += 2
 
 
