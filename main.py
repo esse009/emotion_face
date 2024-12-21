57a5f2f9537496a7effcd4f7bb0ad09fe1b99a1e
esse009 <esse.zeng@outlook.com>
2024-12-21 16:09:09 +0100

    s

--- main.py
+++ main.py
@@ -164,7 +164,9 @@ async def play_audio_after_delay_welcome(client, char, camera, classifier):
     #audio
     await display_emotions.display_emotion(client, char, "happy")
     
-    os.system("aplay '/home/esse/Documents/audio/welcome.wav'")
+    os.spawnv(os.P_NOWAIT, 'aplay', ['/home/esse/Documents/audio/welcome.wav'])
+
+    # os.system("aplay '/home/esse/Documents/audio/welcome.wav'")
      # exppressions: neutral-exciting
     await display_emotions.display_emotion(client, char, "happy")
     # exppressions: neutral-exciting
